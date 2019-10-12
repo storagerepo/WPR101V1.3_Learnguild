91a0dcae503681b817ff4a9efd8f3fc2335d2350
storagerepo <deemsysinchr@gmail.com>
2019-10-12 13:52:33 +0530

    Committing code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learn::Application.config.secret_token = 'aede5dd9d54de7f9bddf464ce38d3d24600e6aed20b9a39a532467ff8cc4a80dab0f1b0d0cec8dedbd188e43faa3a2e95906780c82f664d7eb15211a9a9c8af7'
